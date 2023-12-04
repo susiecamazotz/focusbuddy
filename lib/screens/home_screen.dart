921733e57a0701f44a37f15368adca87bfc0b9ee
Zuzanna Słapek <zuzannaslapek@gmail.com>
2023-12-04 19:01:54 +0100

    updated home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const maxSeconds = 25 * 60;
+  int seconds = maxSeconds;
+
+  Timer? timer;
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (seconds > 0) {
+        setState(() {
+          seconds--;
+        });
+      } else {
+        timer?.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Build your home screen widget here.
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: Center(
-        child: Text('Home Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${(seconds ~/ 60).toString().padLeft(2, '0')}:${(seconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Image.asset(
+                'assets/pets/cat.png'), // Panda icon replaced by cat icon
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => startTimer(),
+              child: Text('START'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // Start button color
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
